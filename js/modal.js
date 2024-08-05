0c6a4f7f4edd7d7e3f192cb2b4b5e5ff50e93f22
Administracion de Redes FISEI <adminredesfisei@uta.edu.ec>
2024-08-05 18:12:28 +0000

    Cambios antes de actualizacion de fechas.

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 // Obtener referencias a los elementos del DOM
-var modal_1 = document.getElementById("modal-1");
+/*var modal_1 = document.getElementById("modal-1");
 var btn_1 = document.getElementById("btn-1");
 var modal_2 = document.getElementById("modal-2");
 var btn_2 = document.getElementById("btn-2");
@@ -18,7 +18,7 @@ var btn_8 = document.getElementById("btn-8");
 var modal_9 = document.getElementById("modal-9");
 var btn_9 = document.getElementById("btn-9");
 var modal_10 = document.getElementById("modal-10");
-var btn_10 = document.getElementById("btn-10");
+var btn_10 = document.getElementById("btn-10");*/
 var modal_req = document.getElementById("modal-req");
 var btn_req = document.getElementById("btn-req");
 var modal_sel = document.getElementById("modal-sel");
@@ -27,7 +27,7 @@ var modal_cup = document.getElementById("modal-cup");
 var btn_cup = document.getElementById("btn-cup");
 
 // Abrir modal al hacer clic en el botón
-btn_1.addEventListener("click", function() {
+/*btn_1.addEventListener("click", function() {
   modal_1.style.display = "block";
 });
 btn_2.addEventListener("click", function() {
@@ -56,7 +56,7 @@ btn_9.addEventListener("click", function() {
 });
 btn_10.addEventListener("click", function() {
   modal_10.style.display = "block";
-});
+});*/
 btn_req.addEventListener("click", function() {
   modal_req.style.display = "block";
 });
@@ -69,7 +69,7 @@ btn_cup.addEventListener("click", function() {
 
 // Cerrar modal al hacer clic fuera del contenido del modal
 window.addEventListener("click", function(event) {
-  if (event.target == modal_1) {
+  /*if (event.target == modal_1) {
     modal_1.style.display = "none";
   }
   if (event.target == modal_2) {
@@ -98,7 +98,7 @@ window.addEventListener("click", function(event) {
   }
   if (event.target == modal_10) {
     modal_10.style.display = "none";
-  }
+  }*/
   if (event.target == modal_req) {
     modal_req.style.display = "none";
   }
@@ -108,4 +108,43 @@ window.addEventListener("click", function(event) {
   if (event.target == modal_cup) {
     modal_cup.style.display = "none";
   }
+});
+
+document.getElementById('btn-bro').addEventListener('click', function() {
+  // URL del archivo PDF
+  const pdfUrl = 'files/brochure.pdf';
+  
+  // Crear un elemento de anclaje
+  const a = document.createElement('a');
+  a.href = pdfUrl;
+  a.target = '_blank'; // Abrir en una nueva pestaña
+  
+  // Simular un clic en el ancla para abrir el PDF en otra pestaña
+  a.click();
+});
+
+document.getElementById('btn-malla').addEventListener('click', function() {
+  // URL del archivo PDF
+  const pdfUrl = 'files/mallacurricular.pdf';
+  
+  // Crear un elemento de anclaje
+  const a = document.createElement('a');
+  a.href = pdfUrl;
+  a.target = '_blank'; // Abrir en una nueva pestaña
+  
+  // Simular un clic en el ancla para abrir el PDF en otra pestaña
+  a.click();
+});
+
+document.getElementById('btn-proceso').addEventListener('click', function() {
+  // URL del archivo PDF
+  const pdfUrl = 'files/req.pdf';
+  
+  // Crear un elemento de anclaje
+  const a = document.createElement('a');
+  a.href = pdfUrl;
+  a.target = '_blank'; // Abrir en una nueva pestaña
+  
+  // Simular un clic en el ancla para abrir el PDF en otra pestaña
+  a.click();
 });
